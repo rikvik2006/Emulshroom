edcc73a3b5e5b8d2b1a32e8b10aba0b74a7c036f
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-15 12:45:02 +0100

    Feat: Created the navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import style from './index.module.scss'
+import Image from "next/image"
 
 type Props = {}
 
@@ -7,9 +8,25 @@ type Props = {}
 const NavbarContainer: FC<Props> = () => {
     return (
         <div className={style.container}>
-            <div className={style.logo}></div>
+            <div className={style.logoContainer}>
+                <Image className={style.logo} alt='logo' src="https://imgur.com/BqPMnCm.png" width={30} height={30} />
+            </div>
             <div className={style.links}>
+                <div className={style.linkContainer}>
+                    <a className="link" href='#'>About</a>
+                </div>
 
+                <div className={style.linkContainer}>
+                    <a className="link" href='#'>Store</a>
+                </div>
+
+                <div className={style.linkContainer}>
+                    <a className="link" href='#'>About</a>
+                </div>
+
+                <div className={style.linkContainer}>
+                    <a className="link" href='#'>Contact</a>
+                </div>
             </div>
         </div>
     )
