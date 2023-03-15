a6e840a2fa2557f32ef7c30b79e10403e1ca78c7
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-15 12:46:34 +0100

    Feat: Created the hero content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
-import styles from "../styles/Home.module.scss"
+import style from "../styles/Home.module.scss"
 import NavbarContainer from '@components/components/navbar'
+import { Montserrat } from "next/font/google"
 
 type Props = {}
 
@@ -12,8 +13,18 @@ const HomePage: NextPage<Props> = () => {
             <Head>
                 <title>Emulsionati House</title>
             </Head>
-            <div className={styles.container}>
+            <div className={style.container}>
                 <NavbarContainer />
+
+                <div className={style.heroContent}>
+                    <div className={style.imageContainer}>
+                        <img src="/img/heroimage.jpg" alt="heroimage" />
+                        <h2>La natura divide noi uniamo</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem fugiat eaque id assumenda blanditiis sit est! Vitae eius deleniti amet debitis placeat rerum, facilis quis. Illo earum deserunt voluptatem?
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     )
