128e8f7b4c901734f1256e4bacbd2bcc9b60d02e
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-23 11:48:20 +0100

    Feat(nav-bar): Modifyed color of the navbar on scroll

--- src/components/heroContent/navbar/index.tsx
+++ src/components/heroContent/navbar/index.tsx
@@ -1,11 +1,27 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import style from "./index.module.scss";
 
 type Props = {};
 
 const NavbarContainer: FC<Props> = () => {
+    const [scroll, setScroll] = useState(false);
+
+    const changeBackground = () => {
+        if (window.scrollY >= 66) {
+            setScroll(true);
+        } else {
+            setScroll(false);
+        }
+    }
+
+    useEffect(() => {
+        changeBackground();
+
+        window.addEventListener("scroll", changeBackground);
+    })
+
     return (
-        <div className={style.container} id="navbar">
+        <div className={[`${style.container}`, `${scroll ? style.scrolled : ""}`].join(" ")} id="navbar">
             <div className={style.leftPart}>
                 <div className={style.logoContainer}>
                     <img src="/img/logo.png" alt="Emulshoorm Logo" />
