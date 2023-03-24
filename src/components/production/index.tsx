4312cb3f9fb13ecd3d2943770b161a1668523732
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-24 10:12:15 +0100

    Fix: For a mistake deleted permanetely a file, recreated

--- src/components/production/index.tsx
+++ src/components/production/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import style from "./index.module.scss"
+import linksStyle from "../../styles/links.module.scss"
 
 type Props = {}
 
@@ -13,6 +14,7 @@ const ProductionContainer: FC<Props> = () => {
                     Our company recognizes the importance of producing our product in a safe and sustainable manner. We are committed to following safety regulations and adopting the most effective practices to reduce the environmental impact of our production.<br />
                     The safety of our workers and customers is a top priority, and we are constantly working to improve working conditions and the quality of our product.
                 </p>
+                <a className={linksStyle.link} href="#">View about our production</a>
             </div >
         </>
     )
