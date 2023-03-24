e61f103d0896a2acf3fe17cb167b1b6c1ece0821
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-24 17:37:46 +0100

    Feat: Added some style features

--- src/components/heroContent/navbar/index.tsx
+++ src/components/heroContent/navbar/index.tsx
@@ -1,16 +1,24 @@
 import { FC, useEffect, useState } from "react";
 import style from "./index.module.scss";
+import { StaticImageData } from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { PageInfos } from "../../../types/hadersType";
 
-type Props = {};
+type Props = {
+
+};
 
 const NavbarContainer: FC<Props> = () => {
-    const [scroll, setScroll] = useState(false);
+    const [scrol, setScrol] = useState(false);
+
+    const router = useRouter();
 
     const changeBackground = () => {
         if (window.scrollY >= 66) {
-            setScroll(true);
+            setScrol(true);
         } else {
-            setScroll(false);
+            setScrol(false);
         }
     }
 
@@ -20,14 +28,22 @@ const NavbarContainer: FC<Props> = () => {
         window.addEventListener("scroll", changeBackground);
     })
 
+    const goToHome = () => {
+        router.push("/");
+    }
+
+    const goToAboutUs = () => {
+        router.push("/about-us");
+    }
+
     return (
-        <div className={[`${style.container}`, `${scroll ? style.scrolled : ""}`].join(" ")} id="navbar">
+        <div className={[`${style.container}`, `${scrol ? style.scrolled : ""}`].join(" ")} id="navbar">
             <div className={style.leftPart}>
                 <div className={style.logoContainer}>
-                    <img src="/img/logo.png" alt="Emulshoorm Logo" />
+                    <img src="/img/logo.png" alt="Emulshoorm Logo" onClick={goToHome} />
                 </div>
 
-                <div className={style.item}>Research</div>
+                <div className={style.item} onClick={goToAboutUs}>Research</div>
                 <div className={style.item}>Product</div>
                 <div className={style.item}>About us</div>
                 <div className={style.item}>Company</div>
@@ -38,7 +54,7 @@ const NavbarContainer: FC<Props> = () => {
                     <div className={style.item}>IT</div>
                 </div> */}
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from "next"
 import style from "../styles/introduction.module.scss"
-import HeroImage from "@components/components/heroContent/heroImage/heroImage"
-import MainContent from "@components/components/mainContent"
-import ProductionContainer from "@components/components/production"
-import ProductsContainer from "@components/components/products"
-import Footer from "@components/components/footer"
+import HeroImage from "@components/heroContent/heroImage/heroImage"
+import MainContent from "@components/mainContent"
+import ProductionContainer from "@components/production"
+import ProductsContainer from "@components/products"
+import Footer from "@components/footer"
 
 type Props = {
 
