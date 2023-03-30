63bf0ce7b6f41b9291cbf62741321c2119e3ecd4
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-30 15:06:22 +0200

    Feat: Creating inter page transition

---
+++ src/helpers/pageTransition.ts
@@ -0,0 +1,9 @@
+window.addEventListener('beforeunload', function (event: BeforeUnloadEvent) {
+    event.preventDefault();
+    const whiteScreen = document.createElement('div');
+    whiteScreen.classList.add('white-screen');
+    document.body.appendChild(whiteScreen);
+    setTimeout(function () {
+        window.location.href = 'nuova-pagina.html';
+    }, 200);
+});
