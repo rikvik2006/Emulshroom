ef0c8a5e3084f73b514b972ce222f9c59e5e42c8
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-10-04 18:09:08 +0200

    chore: Fixed a text content typo

--- src/components/aboutUs/mainContent/sections/controlloQualitaSection.tsx
+++ src/components/aboutUs/mainContent/sections/controlloQualitaSection.tsx
@@ -4,7 +4,7 @@ import style from "./index.module.scss";
 const ContrlloQualitaSection: FC = () => {
     return (
         <div className={style.container}>
-            <h3 className={style.sectionTitle}>Contrllo qualità</h3>
+            <h3 className={style.sectionTitle}>Controllo qualità</h3>
             <div className={style.sectionDescription}>Grazie ad analisi spettrofotometriche a campione, riusciamo a garantire il grado di purezza più elevato (99,9999%).</div>
         </div>
     )
